3976cff6d6784d37620b0922c11a7890a2536025
hushicai <bluthcy@gmail.com>
2016-12-02 16:58:14 +0800

    update

--- weixin/items/article.py
+++ weixin/items/article.py
@@ -2,27 +2,18 @@
 
 import scrapy
 
-class AbstractItem(scrapy.Item):
+class Item(scrapy.Item):
 
-  """微信文章概要"""
+  """微信文章信息"""
 
   title = scrapy.Field()
   abstract = scrapy.Field()
   account_name = scrapy.Field()
-
-  def __init__(self):
-    scrapy.Item.__init__(self)
-
-
-class DetailItem(scrapy.Item):
-
-  """微信文章详情"""
-
-  link = scrapy.Field()
-  content = scrapy.Field()
   publish_time = scrapy.Field(serilizer = str)
+  content = scrapy.Field()
+  author = scrapy.Field()
   read_count = scrapy.Field()
-  like_count = scrapy.Field()
+  praise_count = scrapy.Field()
 
   def __init__(self):
     scrapy.Item.__init__(self)
--- weixin/spiders/weixin_spider.py
+++ weixin/spiders/weixin_spider.py
@@ -1,10 +1,11 @@
 
 #  enocoding: utf-8
 
+import re
 import scrapy
-
+from bs4 import BeautifulSoup
+from urllib import urlencode
 # relative import只能用在toplevel空间中
-
 from weixin.items import *
 
 class WeixinSpider(scrapy.Spider):
@@ -19,24 +20,27 @@ class WeixinSpider(scrapy.Spider):
     parse搜索列表
     """
     for item in response.xpath('//*[@class="news-list"]/li'):
-      title = "".join(item.xpath('div[2]/h3/a/node()').extract())
-      abstract = "".join(item.xpath('div[2]/p/node()').extract())
-      account_name = item.xpath('div[2]/div/a[1]/text()').extract_first()
-      account_temp_link = item.xpath('div[2]/div/a[1]/@href').extract_first()
+      title = "".join(item.xpath('.//h3/a/node()').extract())
+      abstract = "".join(item.xpath('.//p[@class="txt-info"]/node()').extract())
+      #  搜狗时效性链接
+      article_temp_link = item.xpath('.//h3/a/@href').extract_first()
+      #  账号名
+      account_name = item.xpath('.//div[@class="s-p"]/a/text()').extract_first()
+      #  账号临时链接
+      #  account_temp_link = item.xpath('.//div[@class="s-p"]/a/@href').extract_first()
 
-      if account_temp_link is not None:
-        yield scrapy.Request(
-          account_temp_link,
-          callback = self.parse_detail
-        );
+      meta = {
+        'article_title': title,
+        'article_abstract': abstract,
+        'article_account_name': account_name
+      }
 
-
-      abstract_article_item = article.AbstractItem()
-      abstract_article_item['title'] = title
-      abstract_article_item['abstract'] = abstract
-      abstract_article_item['account_name'] = account_name
-
-      yield abstract_article_item
+      if article_temp_link is not None:
+          yield scrapy.Request(
+            article_temp_link,
+            callback = self.parse_detail,
+            meta = meta
+          )
 
     next_page = response\
       .xpath('//div[@id="pagebar_container"]/a[@id="sogou_next"]/@href')\
@@ -46,17 +50,17 @@ class WeixinSpider(scrapy.Spider):
       next_page_url = response.urljoin(next_page)
       yield scrapy.Request(next_page_url, callback = self.parse)
 
-
   def parse_detail(self, response):
-    """parse文章详情页"""
-    account_name = ''
-    link = ''
-    content = ''
-    publish_time = ''
-    read_count = ''
-    like_count = ''
+    """parse文章详情"""
+    meta = response.meta
 
-    yield {
-      'account_name': '',
-      'link': link
-    }
+    article_item = article.Item()
+
+    article_item['account_name'] = meta['article_account_name']
+    article_item['title'] = meta['article_title']
+    article_item['abstract'] = meta['article_abstract']
+    article_item['author'] = response.xpath('//*[@id="img-content"]/div[1]/em[2]/text()').extract_first()
+    article_item['content'] = "".join(response.xpath('//*[@id="js_content"]/node()').extract())
+    article_item['publish_time'] = response.xpath('//*[@id="img-content"]/div[1]/em[1]/text()').extract_first()
+
+    yield article_item
